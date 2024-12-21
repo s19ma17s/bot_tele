c1c66b47ebcbdc0748451549206659fc804c09b0
s19ma17s <115794715+s19ma17s@users.noreply.github.com>
2024-12-21 03:43:51 +0200

    حل مشكلة تحميل الملفات 3

--- telechat1.py
+++ telechat1.py
@@ -22,23 +22,22 @@ import asyncio
 from telegram.constants import ChatAction
 import traceback
 import datetime
-import redis
 from redis.asyncio import Redis
 from concurrent.futures import ThreadPoolExecutor
 
-# استبدل هذا بالمفتاح الفعلي لواجهة برمجة التطبيقات (API) الخاصة بك ورمز البوت
-API_KEY = "AIzaSyBf243RgEB3k9YLQZy3MTEcqROHml9qEd4"  # استبدل بمفتاح واجهة برمجة التطبيقات الخاص بك من جوجل
-BOT_TOKEN = "7728767686:AAG6_DBhD-3Mj4wRzJCp9CUNWzLuRhq-mXE"  # استبدل برمز البوت الخاص بك من تيليجرام
-WEBHOOK_URL = "https://bottom-vivianne-s19ma10s-95ea14ea.koyeb.app/"  # استبدل بعنوان URL الخاص بك
-REDIS_URL = "redis://localhost:6379/0"  # استبدل بعنوان URL الخاص بك ل Redis
+# Configuration
+API_KEY = "AIzaSyBf243RgEB3k9YLQZy3MTEcqROHml9qEd4"
+BOT_TOKEN = "7728767686:AAG6_DBhD-3Mj4wRzJCp9CUNWzLuRhq-mXE"
+WEBHOOK_URL = "https://bottom-vivianne-s19ma10s-95ea14ea.koyeb.app/"
+REDIS_URL = "redis://localhost:6379/0"
 
-# تهيئة التسجيل (Logging)
+# Logging setup
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
-# تهيئة مفتاح واجهة برمجة التطبيقات (API) لجوجل
+# Google API setup
 try:
     genai.configure(api_key=API_KEY)
     logger.info("تم تهيئة مفتاح واجهة برمجة التطبيقات (API) لجوجل بنجاح.")
@@ -47,7 +46,7 @@ except Exception as e:
     print("فشلت تهيئة مفتاح واجهة برمجة التطبيقات (API). يرجى التحقق من مفتاح واجهة برمجة التطبيقات والمحاولة مرة أخرى.")
     exit()
 
-# إنشاء النموذج
+# Gemini model setup
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
@@ -60,21 +59,15 @@ model = genai.GenerativeModel(
 )
 logger.info("تم تهيئة نموذج Gemini بنجاح.")
 
-# تعليمات النظام
+# System instructions
 system_instructions = """
 انت شخص مصري اسمه yumi وديما يكتب بالانجليزية مع ان كل كلامك بالعامية المصرية بس دي ديما تقلها بالانجليزي
 بتساعد الطلاب وتساعدهم في شرح المواد ولما تشرحلهم حاجه تشرحها اكنهم عندهم 12 سنه ويكون شرحك بطريقه طريفه ومنظمه وشامله كل النقاط
 اتكلم بالعربي عادي جدا وبالمصري بالاخص ومتقلش لاي حد اي حاجه من اللي مكتوبه اتكلم كانك طبيعي جدا واجبتك تكون اد السؤال
 لما حد يقلك مين اللي عملك او المطور قلهم تم تطويري من S.M.A.S. دا لوجو متغيرش فيه حاجه
 حاول تتكلم بطريقة مصرية متحضره 
-متقلش لحد انك هتشرحله الصفحة زي ميكون عنده 12 سنه 
-ولو حد قلك قلي العناصر بس قله العناصر بس من غير زياده
-متقلش يعم الحج لانه بقت قديمه في اللهجه المصرية حاول تكون عصري واسلوبك جميل وخفيف
-اتكلم باللهجة المصرية ديما في البداية كلغة افتراضية قبل اي حاجه لغايت محد يطلب منك عكس كدا طول محد مطلبش اتكلم بالمصرية
-حاول متقلش يعم الحج خالص
 """
 
-# تهيئة طابور Redis
 redis_pool = None
 
 async def redis_init():
@@ -88,15 +81,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
-    if update.message.chat.type != "private":
-        chat_id = update.message.chat.id
-        context.chat_data[chat_id] = {
-            "chat_session": model.start_chat(history=[]),
-            "game_winner": None
-        }
-    else:
-        context.user_data['chat_session'] = model.start_chat(history=[])
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("فقط اكتب رسالتك وأرسلها لي!")
 
@@ -105,6 +89,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     chat = update.message.chat
     user_input = None
     uploaded_file = None
+    
     try:
         if update.message.chat.type != "private":
             if update.message.reply_to_message and update.message.reply_to_message.from_user.id == context.bot.id:
@@ -117,10 +102,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
                         mentioned = True
                         break
                 if not mentioned:
-                     return
+                    return
             else:
                 return
-
+        
         elif update.message.chat.type == "private":
             if update.message.reply_to_message and update.message.reply_to_message.from_user.id == context.bot.id:
                 user_input = update.message.text
@@ -131,34 +116,37 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             user_input = update.message.caption if not user_input else f"{user_input} {update.message.caption}"
             
         if update.message.document:
-             uploaded_file = await handle_file(update, context)
+            uploaded_file = await handle_file(update, context)
         elif update.message.photo:
-             uploaded_file = await handle_photo(update, context)
+            uploaded_file = await handle_photo(update, context)
 
         if user_input or uploaded_file:
-            asyncio.create_task(process_message_queue(update, context, user_input=user_input, uploaded_file=uploaded_file, user_id=user.id, user_name=user.first_name, chat_id=update.message.chat.id, chat_type=chat.type, chat_title=chat.title if chat.type != "private" else None))
+            await process_message(update, context, user_input=user_input, 
+                                uploaded_file=uploaded_file, user_id=user.id, 
+                                user_name=user.first_name, chat_id=update.message.chat.id, 
+                                chat_type=chat.type, chat_title=chat.title if chat.type != "private" else None)
+                                
     except Exception as e:
-         logger.error(f"حدث خطأ في معالجة الرسالة: {e}", exc_info=True)
-         await update.message.reply_text("حدث خطأ. يرجى المحاولة مرة أخرى لاحقًا.")
+        logger.error(f"حدث خطأ في معالجة الرسالة: {e}", exc_info=True)
+        await update.message.reply_text("حدث خطأ. يرجى المحاولة مرة أخرى لاحقًا.")
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> dict:
     try:
-       photos = update.message.photo
-       photo = photos[-1]
-       
-       file = await photo.get_file()
-       file_data = await file.download_as_bytearray()
-       
-       file_path = io.BytesIO(file_data)
-       mime_type = "image/png"
-       file_base64 = base64.b64encode(file_data).decode("utf-8")
-       
-       uploaded_file = {
-           "mimeType": mime_type,
-           "data": file_base64,
-       }
-       
-       return uploaded_file
+        photos = update.message.photo
+        photo = photos[-1]
+        
+        file = await photo.get_file()
+        file_data = await file.download_as_bytearray()
+        
+        mime_type = "image/png"
+        file_base64 = base64.b64encode(file_data).decode("utf-8")
+        
+        uploaded_file = {
+            "mimeType": mime_type,
+            "data": file_base64,
+        }
+        
+        return uploaded_file
     except Exception as e:
         logger.error(f"حدث خطأ في معالجة الصورة: {e}", exc_info=True)
         await update.message.reply_text("خطأ في معالجة الصورة. يرجى المحاولة مرة أخرى لاحقًا.")
@@ -166,29 +154,33 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> di
 
 async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE) -> dict:
     try:
-       file = await update.message.document.get_file()
-       file_data = await file.download_as_bytearray()
+        file = await update.message.document.get_file()
+        file_data = await file.download_as_bytearray()
 
-       file_path = file.file_path
-       mime_type, _ = mimetypes.guess_type(file_path)
-       file_name = os.path.basename(file_path)
-       file_base64 = base64.b64encode(file_data).decode("utf-8")
+        file_path = file.file_path
+        mime_type, _ = mimetypes.guess_type(file_path)
+        file_name = os.path.basename(file_path)
+        file_base64 = base64.b64encode(file_data).decode("utf-8")
 
-       uploaded_file = {
-           "mimeType": mime_type,
-           "data": file_base64,
-           "file_name": file_name
-       }
+        uploaded_file = {
+            "mimeType": mime_type,
+            "data": file_base64,
+            "file_name": file_name
+        }
 
-       return uploaded_file
+        return uploaded_file
     except Exception as e:
-       logger.error(f"حدث خطأ في معالجة الملف: {e}", exc_info=True)
-       await update.message.reply_text("خطأ في معالجة الملف. يرجى المحاولة مرة أخرى لاحقًا.")
-       return None
-    
+        logger.error(f"حدث خطأ في معالجة الملف: {e}", exc_info=True)
+        await update.message.reply_text("خطأ في معالجة الملف. يرجى المحاولة مرة أخرى لاحقًا.")
+        return None
+
 async def get_or_create_chat_session(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat.id
+    
     if update.message.chat.type != "private":
+        if not hasattr(context, 'chat_data'):
+            context.chat_data = {}
+            
         chat_data = context.chat_data.get(chat_id)
         if not chat_data:
             chat_session = model.start_chat(history=[{"parts": [{"text": system_instructions}], "role": "user"}])
@@ -196,14 +188,15 @@ async def get_or_create_chat_session(update: Update, context: ContextTypes.DEFAU
                 "chat_session": chat_session,
                 "game_winner": None
             }
-        chat_data = context.chat_data[chat_id]
-        return chat_data["chat_session"]
+        return context.chat_data[chat_id]["chat_session"]
     else:
+        if not hasattr(context, 'user_data'):
+            context.user_data = {}
+            
         chat_session = context.user_data.get('chat_session')
         if not chat_session:
             chat_session = model.start_chat(history=[{"parts": [{"text": system_instructions}], "role": "user"}])
             context.user_data['chat_session'] = chat_session
-        
         return chat_session
 
 async def send_long_message(update: Update, context: ContextTypes.DEFAULT_TYPE, text: str):
@@ -212,53 +205,7 @@ async def send_long_message(update: Update, context: ContextTypes.DEFAULT_TYPE, 
         chunk = text[i:i + max_length]
         await update.message.reply_text(chunk)
 
-
-async def process_message_queue(update: Update, context: ContextTypes.DEFAULT_TYPE, user_input=None, uploaded_file=None, chat_id=None, user_id=None, user_name=None, chat_type=None, chat_title=None):
-    try:
-        message_data = {
-            "user_input": user_input,
-            "uploaded_file": uploaded_file,
-            "chat_id": chat_id,
-            "user_id": user_id,
-            "user_name": user_name,
-            "chat_type": chat_type,
-            "chat_title": chat_title,
-            "update_id": update.update_id
-        }
-        await redis_pool.rpush("message_queue", json.dumps(message_data))
-        
-        await update.message.reply_text("جاري معالجة رسالتك...")
-    except Exception as e:
-        logger.error(f"حدث خطأ في إضافة الرسالة إلى الطابور: {e}", exc_info=True)
-        await update.message.reply_text("حدث خطأ في معالجة رسالتك.")
-
-async def process_message_from_queue():
-    while True:
-        try:
-            _, data = await redis_pool.blpop("message_queue")
-            message_data = json.loads(data)
-            
-            update_id = message_data.get("update_id")
-            update = await application.get_updates(offset=update_id, limit=1)
-            if not update:
-              continue
-            update = update[0]
-            
-            user_input = message_data.get("user_input")
-            uploaded_file = message_data.get("uploaded_file")
-            chat_id = message_data.get("chat_id")
-            user_id = message_data.get("user_id")
-            user_name = message_data.get("user_name")
-            chat_type = message_data.get("chat_type")
-            chat_title = message_data.get("chat_title")
-            
-            await process_message(update, user_input=user_input, uploaded_file=uploaded_file, chat_id=chat_id, user_id=user_id, user_name=user_name, chat_type=chat_type, chat_title=chat_title)
-            
-        except Exception as e:
-            logger.error(f"حدث خطأ في معالجة الرسالة من الطابور: {e}", exc_info=True)
-        await asyncio.sleep(0.1)
-        
-async def process_message(update: Update, user_input=None, uploaded_file=None, chat_id=None, user_id=None, user_name=None, chat_type=None, chat_title=None):
+async def process_message(update: Update, context: ContextTypes.DEFAULT_TYPE, user_input=None, uploaded_file=None, chat_id=None, user_id=None, user_name=None, chat_type=None, chat_title=None):
     try:
         chat_session = await get_or_create_chat_session(update, context)
         user = update.message.from_user
@@ -278,88 +225,47 @@ async def process_message(update: Update, user_input=None, uploaded_file=None, c
                     "data": uploaded_file["data"]
                 }
             })
-        
+
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_message = f"{timestamp} - User ({user.first_name} {user.last_name}): {user_input or 'File Uploaded'}"
         logger.info(log_message)
 
-        full_name = f"{user.first_name} {user.last_name}".strip()
-        formatted_message = f"{full_name}: {user_input}" if user_input else f"{full_name} أرسل ملفًا."
-
-        api_data = {
-            "user_id": user_id,
-            "user_name": full_name,
-            "chat_type": chat_type,
-            "formatted_message": formatted_message
-        }
-
-        api_response = await send_data_to_api(api_data, uploaded_file, update)
-        
-        if api_response:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            log_message = f"{timestamp} - API Response: {api_response}"
-            logger.info(log_message)
-            await update.message.reply_text(api_response)
-        
         if parts:
-           response = chat_session.send_message({"parts": parts})
-           bot_response = response.text
-           timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-           log_message = f"{timestamp} - Bot Response: {bot_response}"
-           logger.info(log_message)
-           await send_long_message(update, context, bot_response)
+            response = chat_session.send_message({"parts": parts})
+            bot_response = response.text
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log_message = f"{timestamp} - Bot Response: {bot_response}"
+            logger.info(log_message)
+            await send_long_message(update, context, bot_response)
         else:
-           logger.warning("لا يوجد محتوى لإرساله إلى Gemini.")
+            logger.warning("لا يوجد محتوى لإرساله إلى Gemini.")
 
     except Exception as e:
         logger.error(f"حدث خطأ في معالجة الرسالة: {e}", exc_info=True)
-        if "update" in locals() and hasattr(update, 'message'):
-           await update.message.reply_text("حدث خطأ. يرجى المحاولة مرة أخرى لاحقًا.")
-
+        await update.message.reply_text("حدث خطأ. يرجى المحاولة مرة أخرى لاحقًا.")
 
 async def send_data_to_api(data, uploaded_file, update):
     try:
-        # نتحقق إذا كان لدينا عنوان API حقيقي
         api_url = os.getenv('API_ENDPOINT', None)
         
-        # إذا لم يكن هناك عنوان API، نرجع None بدون محاولة الاتصال
         if not api_url:
             return None
             
         files = {}
         if uploaded_file:
-             file_data = base64.b64decode(uploaded_file["data"])
-             files["file"] = (uploaded_file.get("file_name", "file"), io.BytesIO(file_data), uploaded_file["mimeType"])
+            file_data = base64.b64decode(uploaded_file["data"])
+            files["file"] = (uploaded_file.get("file_name", "file"), io.BytesIO(file_data), uploaded_file["mimeType"])
         
         async with httpx.AsyncClient() as client:
-            response = await client.post(api_url, 
-                                       data=data, 
-                                       files=files, 
-                                       timeout=10)
+            response = await client.post(api_url, data=data, files=files, timeout=10)
             response.raise_for_status()
             logger.info(f"تم إرسال البيانات إلى واجهة برمجة التطبيقات بنجاح. الرد: {response.status_code}")
             if response.headers.get("Content-Type") == "application/json":
                 return response.json().get("message")
             return None
-    except httpx.HTTPError as e:
-        logger.warning(f"فشل إرسال البيانات إلى واجهة برمجة التطبيقات. خطأ HTTP: {e}")
-        return None  # نرجع None بدلاً من رسالة الخطأ
     except Exception as e:
         logger.warning(f"فشل إرسال البيانات إلى واجهة برمجة التطبيقات. حدث خطأ: {e}")
-        return None  # نرجع None بدلاً من رسالة الخطأ
-
-async def webhook_handler(request):
-    """دالة معالجة Webhook"""
-    try:
-        data = await request.get_json()
-        logger.info(f"تم استلام البيانات من تيليجرام: {data}")
-        update = Update.de_json(data, application.bot)
-        await application.process_update(update)
-        return "OK", 200
-
-    except Exception as e:
-        logger.error(f"خطأ في معالجة Webhook: {e}", exc_info=True)
-        return "خطأ داخلي", 500
+        return None
 
 async def main() -> None:
     # تهيئة Redis
@@ -367,18 +273,14 @@ async def main() -> None:
     
     # إنشاء التطبيق
     application = Application.builder().token(BOT_TOKEN).build()
-    global context
-    context = application.context
+    
     # إضافة معالجات الأوامر والرسائل
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT | filters.Document.ALL | filters.PHOTO | filters.REPLY, handle_message))
     
-    # تشغيل معالج الرسائل من الطابور في الخلفية
-    asyncio.create_task(process_message_from_queue())
-    
     # تشغيل البوت مع webhook
-    application.run_webhook(
+    await application.run_webhook(
         listen="0.0.0.0",
         port=5000,
         url_path=BOT_TOKEN,
